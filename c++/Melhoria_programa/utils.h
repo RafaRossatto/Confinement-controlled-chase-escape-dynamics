e73900c1ea163f27d44c79fa4b06fa41020c5d7f
Rafael Rossatto <rafarossatto25@gmail.com>
2025-06-06 11:21:57 -0300

    Estética.

--- c++/Melhoria_programa/utils.h
+++ c++/Melhoria_programa/utils.h
@@ -28,34 +28,6 @@ inline bool indiceValido(int indice, int tamanho)
 }
 
 
-inline void moveTowardsPoint(int &x, int &y, int targetX, int targetY) 
-{
-    // Se a posição X ainda não está alinhada com o alvo, mover primeiro no eixo X
-    if (x != targetX) 
-    {
-        if (x < targetX) 
-        {
-            x = (x + 1) % WIDTH; // Move para a direita
-        } 
-        else 
-        {
-            x = (x - 1 + WIDTH) % WIDTH; // Move para a esquerda
-        }
-    }
-    // Se a posição X já está alinhada, mover no eixo Y
-    else if (y != targetY) 
-    {
-        if (y < targetY) 
-        {
-            y = (y + 1) % HEIGHT; // Move para cima
-        } 
-        else 
-        {
-            y = (y - 1 + HEIGHT) % HEIGHT; // Move para baixo
-        }
-    }
-}
-
 inline void openFile(const std::string& fileName) 
 {
     // Abre o arquivo em modo de escrita e apêndice
