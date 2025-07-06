830272224ad532ef1fb1075b652c12b0a06543b3
Rafael Rossatto <rafarossatto25@gmail.com>
2025-07-06 18:03:24 -0300

    Fiz os gráficos e mandei para os professores.

--- python/analises/projeto/analise_SR_fixado.py
+++ python/analises/projeto/analise_SR_fixado.py
@@ -18,26 +18,26 @@ base_path = Path.home() / "Dados_Doc" / "CLI"/"Teste"
 
 
 # Gera o gráfico do numero de escapers por tempo
-df_NE_10_NC_5= carregar_dataframes(base_path, NE = 10, TCC= 0.99, TCT=0.99,O=0,NC = 5, usar_cache=True, forcar_recarregar=False)
-df_NE_10_NC_10= carregar_dataframes(base_path, NE = 10, TCC= 0.99, TCT=0.99,O=0,NC = 10, usar_cache=True, forcar_recarregar=False)
-df_NE_10_NC_25= carregar_dataframes(base_path, NE = 10, TCC= 0.99, TCT=0.99,O=0,NC = 25, usar_cache=True, forcar_recarregar=False)
-df_NE_10_NC_50= carregar_dataframes(base_path, NE = 10, TCC= 0.99, TCT=0.99,O=0,NC = 50, usar_cache=True, forcar_recarregar=False)
-df_NE_10_NC_100= carregar_dataframes(base_path, NE = 10, TCC= 0.99, TCT=0.99,O=0,NC = 100, usar_cache=True, forcar_recarregar=False)
-df_NE_10_NC_250= carregar_dataframes(base_path, NE = 10, TCC= 0.99, TCT=0.99,O=0,NC = 250, usar_cache=True, forcar_recarregar=False)
-df_NE_10_NC_500= carregar_dataframes(base_path, NE = 10, TCC= 0.99, TCT=0.99,O=0,NC = 500, usar_cache=True, forcar_recarregar=False)
-df_NE_10_NC_1000= carregar_dataframes(base_path, NE = 10, TCC= 0.99, TCT=0.99,O=0,NC = 1000, usar_cache=True, forcar_recarregar=False)
-#df_NE_10_NC_5000= carregar_dataframes(base_path, NE = 10, TCC= 0.99, TCT=0.99,O=0,NC = 5000, usar_cache=True, forcar_recarregar=False)
+df_NE_50_NC_5= carregar_dataframes(base_path, NE = 50, TCC= 0.99, TCT=0.99,O=0,NC = 5, usar_cache=True, forcar_recarregar=False)
+df_NE_50_NC_10= carregar_dataframes(base_path, NE = 50, TCC= 0.99, TCT=0.99,O=0,NC = 10, usar_cache=True, forcar_recarregar=False)
+df_NE_50_NC_25= carregar_dataframes(base_path, NE = 50, TCC= 0.99, TCT=0.99,O=0,NC = 25, usar_cache=True, forcar_recarregar=False)
+df_NE_50_NC_50= carregar_dataframes(base_path, NE = 50, TCC= 0.99, TCT=0.99,O=0,NC = 50, usar_cache=True, forcar_recarregar=False)
+df_NE_50_NC_100= carregar_dataframes(base_path, NE = 50, TCC= 0.99, TCT=0.99,O=0,NC = 100, usar_cache=True, forcar_recarregar=False)
+df_NE_50_NC_250= carregar_dataframes(base_path, NE = 50, TCC= 0.99, TCT=0.99,O=0,NC = 250, usar_cache=True, forcar_recarregar=False)
+df_NE_50_NC_500= carregar_dataframes(base_path, NE = 50, TCC= 0.99, TCT=0.99,O=0,NC = 500, usar_cache=True, forcar_recarregar=False)
+df_NE_50_NC_1000= carregar_dataframes(base_path, NE = 50, TCC= 0.99, TCT=0.99,O=0,NC = 1000, usar_cache=True, forcar_recarregar=False)
+#df_NE_50_NC_5000= carregar_dataframes(base_path, NE = 50, TCC= 0.99, TCT=0.99,O=0,NC = 5000, usar_cache=True, forcar_recarregar=False)
 
 dfs_por_nc = {
-    5: df_NE_10_NC_5,
-    10: df_NE_10_NC_10,
-    25: df_NE_10_NC_25,
-    50: df_NE_10_NC_50,
-    100: df_NE_10_NC_100,
-    250: df_NE_10_NC_250,
-    500: df_NE_10_NC_500,
-    1000: df_NE_10_NC_1000,
-    #5000: df_NE_10_NC_5000,
+    5: df_NE_50_NC_5,
+    10: df_NE_50_NC_10,
+    25: df_NE_50_NC_25,
+    50: df_NE_50_NC_50,
+    100: df_NE_50_NC_100,
+    250: df_NE_50_NC_250,
+    500: df_NE_50_NC_500,
+    1000: df_NE_50_NC_1000,
+    #5000: df_NE_50_NC_5000,
 }
 
 #plotar_media_steps_por_NC(dfs_por_nc, titulo="number of initial escapees $N^{O}_{E}=25$", salvar=True, nome_arquivo="N_E_25.pdf")
